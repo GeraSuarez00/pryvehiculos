17e3dabea1ff4857211e4a9015343feb0a724b06
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2022-04-27 14:14:42 -0500

    modifiqué el admin de ventas

--- apps/ventas/admin.py
+++ apps/ventas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from apps.ventas.models import Venta, VehiculoVenta
 
-# Register your models here.
+class MembershipInline(admin.TabularInline):
+    model = VehiculoVenta
+    extra = 1 #indico la cantidad de campos que quiero para insertar en el formulario, si yo comento esta línea me pone 3 campos por defecto
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('fecha','ValorTotal','tipoPago','user')#muestra los campos que indico y su contenido
+    # ordering = ('fecha','user') #ordena los campos en el orden q indica
+    # search_fields = ('fecha','user') #campos por los que quiero que se busque, cuando es llave foránea pongo dos guiones como en marca__nombre
+    # list_filter= ('fecha','ValorTotal','tipoPago','user') #se coloca al lado derecho lo que se quiere filtrar
+    inlines = (MembershipInline, ) #Coloca un formulario dentro de otro
+admin.site.register(Venta,VentaAdmin) #Aquí registro lo que quiero que se observe en el navegador
+
--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from apps.vehiculos.models import Vehiculo
 # Create your models here.
 
 class Venta(models.Model):
-    fecha =models.DateField()
+    fecha = models.DateField()
     ValorTotal =models.BigIntegerField()
     tipoPago =models.CharField(max_length=20)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    vehiculo = models.ManyToManyField(Vehiculo, through='VehiculoVenta') #through indica el modelo de esa relacion, le puse el nombre de VehiculoVenta y debo crear esa clase
+
+ 
+
+class VehiculoVenta(models.Model):
+    vehiculo = models.ForeignKey(Vehiculo, blank=False, null=False, on_delete=models.CASCADE)#Cuando la relacion es de unos a muchos le pongo foreign key
+    venta = models.ForeignKey(Venta, blank=False, null=False, on_delete=models.CASCADE)#Cuando la relacion es de unos a muchos le pongo foreign key
+    cantidad= models.IntegerField() #aparte de los campos obligatorios: llaves foráneas, puedo agregar mas campos como precio y cantidad
+    precio=models.BigIntegerField()
