ff38fa9069adeeb0e92c23f1630699e5a312f046
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2022-04-27 14:36:01 -0500

    añadí propiedades de filtro, orden y busqueda en admin de ventas

--- apps/ventas/admin.py
+++ apps/ventas/admin.py
@@ -7,9 +7,9 @@ class MembershipInline(admin.TabularInline):
 
 class VentaAdmin(admin.ModelAdmin):
     list_display = ('fecha','ValorTotal','tipoPago','user')#muestra los campos que indico y su contenido
-    # ordering = ('fecha','user') #ordena los campos en el orden q indica
-    # search_fields = ('fecha','user') #campos por los que quiero que se busque, cuando es llave foránea pongo dos guiones como en marca__nombre
-    # list_filter= ('fecha','ValorTotal','tipoPago','user') #se coloca al lado derecho lo que se quiere filtrar
+    ordering = ('fecha','user') #ordena los campos en el orden q indica
+    search_fields = ('fecha',) #campos por los que quiero que se busque, cuando es llave foránea pongo dos guiones como en marca__nombre
+    list_filter= ('fecha','ValorTotal','tipoPago','user') #se coloca al lado derecho lo que se quiere filtrar
     inlines = (MembershipInline, ) #Coloca un formulario dentro de otro
 admin.site.register(Venta,VentaAdmin) #Aquí registro lo que quiero que se observe en el navegador
 
